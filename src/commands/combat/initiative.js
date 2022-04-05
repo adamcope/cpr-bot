123e5c3e36316db89bab9595f14275014f7df832
adamcope <adam@conquestofthread.com>
2022-04-05 02:18:04 -0500

    initiative

--- src/commands/combat/initiative.js
+++ src/commands/combat/initiative.js
@@ -3,28 +3,30 @@ const mongoose = require("mongoose");
 const Character = require("../../models/playerCharacter.js");
 const { MessageEmbed } = require("discord.js");
 const { bold, underscore } = require("@discordjs/builders");
-const statCheck = require("../../modules/statD10.js")
-
+const statCheck = require("../../modules/statD10.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("initiative")
     .setDescription("Roll Initiative"),
   async execute(interaction) {
-    const pc = await Character.findOne({userID:`${interaction.member.id}`}).lean();
+    const pc = await Character.findOne({
+      userID: `${interaction.member.id}`,
+    }).lean();
 
-    const statD10 = statCheck(pc, 'ref')
-
+    const statD10 = statCheck(pc, "ref");
 
     const characterEmbed = new MessageEmbed()
       .setColor("#7a1212")
-      .setTitle(`${pc.characterName} - ${bold('Inititiative')}`)
-      .setDescription(`${bold('Roll:')} ${statD10.display}`)
+      .setTitle(`${pc.characterName} - ${bold("Inititiative")}`)
+      .setDescription(`${bold("Roll:")} ${statD10.display}`)
       .setThumbnail(`${pc.characterImgUrl}`)
       .setFooter({ text: `Player: @${pc.username}` })
-      .addFields(
-        { name: `${underscore('Result')}`, value: `${statD10.result}`, inline: true }
-        );
+      .addFields({
+        name: `${underscore("Result")}`,
+        value: `${statD10.result}`,
+        inline: true,
+      });
 
     await interaction.reply({ embeds: [characterEmbed] });
   },
