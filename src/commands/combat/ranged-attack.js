3dec2855e76b6a17606ed15820d318922e9d0d1c
adamcope <adam@conquestofthread.com>
2022-04-06 22:26:22 -0500

    commit

--- src/commands/combat/ranged-attack.js
+++ src/commands/combat/ranged-attack.js
@@ -18,15 +18,15 @@ module.exports = {
     .addIntegerOption((option) =>
       option
         .setName("distance")
-        .setDescription("Distance to target.")
+        .setDescription("Distance to target in m/yds.")
         .setRequired(true)
     )
     .addStringOption((option) =>
       option
-        .setName("special-modes")
-        .setDescription("Special fire modes.")
+        .setName("altfire-modes")
+        .setDescription("Altfire modes.")
         .addChoice("Autofire", "autofire")
-        .addChoice("Suppressive-fire", "suppressive-fire")
+        .addChoice("Suppressive-Fire", "suppressive-fire")
         .setRequired(false)
     )
     .addStringOption((option) =>
@@ -49,7 +49,7 @@ module.exports = {
 
     const weaponInput = interaction.options.getString("weapon").split(" ");
     const distanceInput = interaction.options.getInteger("distance");
-    const specModes = interaction.options.getString("special-modes");
+    const specModes = interaction.options.getString("altfire-modes");
     const target = interaction.options.getString("aim");
 
     const ra = rangedAttack(pc, weaponInput, distanceInput);
@@ -67,15 +67,17 @@ module.exports = {
       const sc = skillCheck(pc, ["autofire"]);
       const afDmg = attackDmg("2d6");
       const afDmgMultiplier = sc.roll.result - ra.dv;
-      const multiplierLimit = ra.weapon.afMultiplier
+      const multiplierLimit = ra.weapon.afMultiplier;
 
       const multiplier = (x) => {
-          if (x > multiplierLimit){
-              return multiplierLimit
-          } else {return x}
-      }
-      
-      const dmgMult = multiplier(afDmgMultiplier)
+        if (x > multiplierLimit) {
+          return multiplierLimit;
+        } else {
+          return x;
+        }
+      };
+
+      const dmgMult = multiplier(afDmgMultiplier);
 
       const autofireHit = new MessageEmbed()
         .setColor("#7a1212")
@@ -118,14 +120,14 @@ module.exports = {
       const critInjury = new MessageEmbed()
         .setColor("DARK_ORANGE")
         .setTitle(`Critical Injury - ${italic(afDmg.injury.name)}`)
-        .addFields(
-            {
-                name: `${underscore("Effect")}`,
-                value: `${afDmg.injury.effect}`,
-                inline: false,
-              },
+        .addFields({
+          name: `${underscore("Effect")}`,
+          value: `${afDmg.injury.effect}`,
+          inline: false,
+        })
+        .setThumbnail(
+          `https://64.media.tumblr.com/38c2289f4fb32da7afa9e3b4c1eba656/tumblr_nv25v6qn7b1ud4rmfo1_400.gif`
         )
-        .setThumbnail(`https://64.media.tumblr.com/38c2289f4fb32da7afa9e3b4c1eba656/tumblr_nv25v6qn7b1ud4rmfo1_400.gif`)
         .setFooter({ text: `Player: @${pc.username}` });
 
       const autofireMiss = new MessageEmbed()
@@ -152,11 +154,9 @@ module.exports = {
         .setFooter({ text: `Player: @${pc.username}` });
       if (afDmgMultiplier < 1) {
         await interaction.reply({ embeds: [autofireMiss] });
-      }
-      else if (afDmgMultiplier > 0 && afDmg.isCrit == true) {
+      } else if (afDmgMultiplier > 0 && afDmg.isCrit == true) {
         await interaction.reply({ embeds: [autofireHit, critInjury] });
-      }
-      else if (afDmgMultiplier > 0) {
+      } else if (afDmgMultiplier > 0) {
         await interaction.reply({ embeds: [autofireHit] });
       }
     } else if (specModes == "autofire" && ra.weapon.isAutofire == false) {
@@ -246,19 +246,81 @@ module.exports = {
         .addFields(
           {
             name: `${underscore("Roll")}`,
-            value: `[ROLL]`,
-            inline: true,
+            value: `${ra.sc.roll.display}`,
+            inline: false,
           },
           {
             name: `${underscore("Result")}`,
-            value: `[RESULT]`,
+            value: `${ra.sc.roll.result}`,
+            inline: true,
+          },
+          {
+            name: `${underscore("DV")}`,
+            value: `${ra.dv}`,
+            inline: true,
+          },
+          {
+            name: `${underscore("DMG")}`,
+            value: `${ra.dmg.result}`,
             inline: false,
+          },
+          {
+            name: `${underscore("Ammo")}`,
+            value: `${ra.weapon.ammo.loaded}`,
+            inline: true,
+          },
+          {
+            name: `${underscore("Effect")}`,
+            value: `${ra.weapon.ammo.effect}`,
+            inline: true,
           }
         )
         .setThumbnail(`${pc.characterImgUrl}`)
         .setFooter({ text: `Player: @${pc.username}` });
 
-      await interaction.reply({ embeds: [rangedAttackEmbed] });
+      const critInjury = new MessageEmbed()
+        .setColor("DARK_ORANGE")
+        .setTitle(`Critical Injury - ${italic(ra.dmg.injury.name)}`)
+        .addFields({
+          name: `${underscore("Effect")}`,
+          value: `${ra.dmg.injury.effect}`,
+          inline: false,
+        })
+        .setThumbnail(
+          `https://64.media.tumblr.com/38c2289f4fb32da7afa9e3b4c1eba656/tumblr_nv25v6qn7b1ud4rmfo1_400.gif`
+        )
+        .setFooter({ text: `Player: @${pc.username}` });
+
+      const missEmbed = new MessageEmbed()
+        .setColor("#7a1212")
+        .setTitle(`${pc.characterName} - ${italic("Ranged Attack - Autofire")}`)
+        .addFields(
+          {
+            name: `${underscore("Roll")}`,
+            value: `${ra.sc.roll.display}`,
+            inline: false,
+          },
+          {
+            name: `${underscore("Result")}`,
+            value: `${ra.sc.roll.result}`,
+            inline: true,
+          },
+          {
+            name: `${underscore("DV")}`,
+            value: `${ra.dv}`,
+            inline: true,
+          }
+        )
+        .setThumbnail(`${pc.characterImgUrl}`)
+        .setFooter({ text: `Player: @${pc.username}` });
+
+      if (ra.dmg.isCrit == true && ra.isHit == true) {
+        await interaction.reply({ embeds: [rangedAttackEmbed, critInjury] });
+      } else if (ra.dmg.isCrit == false && ra.isHit == true) {
+        await interaction.reply({ embeds: [rangedAttackEmbed] });
+      } else if (ra.isHit == false) {
+        await interaction.reply({ embeds: [missEmbed] });
+      }
     }
   },
 };
