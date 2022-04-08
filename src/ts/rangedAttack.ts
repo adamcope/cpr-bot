73dfd409a4316e92e73dc1e816f3c421a23cc284
adamcope <adam@conquestofthread.com>
2022-04-08 15:37:09 -0500

    ranged attack mongoDB ammo update

--- src/ts/rangedAttack.ts
+++ src/ts/rangedAttack.ts
@@ -1,48 +1,47 @@
-
-const { hitCheck, dvIndex, attackDmg } =  require( "../modules/mechanics");
+const { hitCheck, dvIndex, attackDmg } = require("../modules/mechanics");
 const skillCheck = require("../modules/skillCheck");
 const lib = require("./library");
 
 /**
  * @param {typeof PC} pc ``await Character.findOne({userID:`${interaction.member.id}`}).lean();``
- * @param {number} distance ``interaction.options.getInteger("distance")`` 
+ * @param {number} distance ``interaction.options.getInteger("distance")``
  */
 function rangedAttack(pc: typeof PC, weapon: Array<string>, distance: number) {
-  
-  const weaponRef: string = weapon.join("_").toLowerCase()
-  
+  const weaponRef: string = weapon.join("_").toLowerCase();
+
   try {
-    
-    const weaponInfo: typeof PC.weapons[0] = pc.weapons.find((x: typeof Weapon) => x.ref == weaponRef)! 
-    //** add a try - catch for weapons not equipped.
-    const ammoInfo = lib.ammo.find((x: any) => x.name == weaponInfo.ammoLoaded)
-    const weaponSkill: Array<string> = weaponInfo.skillName.toLowerCase().split(" ");
+    const weaponInfo = pc.weapons.find((x: any) => x.ref == weaponRef)!;
+
+    const ammoInfo = lib.ammo.find((x: any) => x.name == weaponInfo.ammoLoaded);
+    const weaponSkill: Array<string> = weaponInfo.skillName
+      .toLowerCase()
+      .split(" ");
     const index: number = dvIndex(distance);
-    
+
     const sc: SC = skillCheck(pc, weaponSkill);
     const dv: number = weaponInfo.rangeDV[index];
-    
+
     const atk = {
       sc: sc,
       dv: dv,
       weapon: {
         name: weaponInfo.name,
         rof: weaponInfo.rof,
-        isAutofire: weaponInfo.autofire[0],
-        afMultiplier: weaponInfo.autofire[1],
-        ammo: { 
+        isAutofire: weaponInfo.isAutofire,
+        afMultiplier: weaponInfo.afMultiplier,
+        ammo: {
           count: weaponInfo.ammo,
           loaded: weaponInfo.ammoLoaded,
-          effect: ammoInfo.effect
-        }
+          effect: ammoInfo.effect,
+        },
       },
       isHit: hitCheck(dv, sc.roll.result),
       dmg: attackDmg(weaponInfo.dmg),
     };
-    
+
     return atk;
   } catch (error) {
-    return undefined
+    return undefined;
   }
 }
 
